a3fbe4414ce65dc5f0a1d5e518f7fe4b69b58844
Scott Walters <scottwalters97@gmail.com>
2017-07-03 12:01:12 +1000

    Pushing out ver 4.5 before a re-write

--- temp.sh
+++ temp.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-# ver. 4
+echo "~~ nan0s7's fan control script [version 4.5] ~~"
+
+# -- notes --
+# speeds are in percentages
 
 # Variables
 LOOP=""
 GPU="0"
-NOW_SPEED=30
+NOW_SPEED="30"
+SPEED="0"
 
 # Enable fan control
 nvidia-settings -a "[gpu:""$GPU""]/GPUFanControlState=1"
 
 while [ -z "$LOOP" ]; do
+	# Current temperature query
 	TEMP=`nvidia-settings -q=[gpu:"$GPU"]/GPUCoreTemp`
 
+	# Extracts temperature value from output of command
 	if [ "${TEMP:54:1}" == "." ]; then
 		TEMP="${TEMP:53:1}"
 	elif [ "${TEMP:55:1}" == "." ]; then
@@ -20,24 +26,29 @@ while [ -z "$LOOP" ]; do
 		TEMP="${TEMP:53:3}"
 	fi
 
+	# You can edit these values if you like
 	if [ "$TEMP" -le "40" ]; then
-		SPEED=40
+		SPEED="40"
 	elif [ "$TEMP" -le "45" ]; then
-		SPEED=55
+		SPEED="55"
 	elif [ "$TEMP" -le "50" ]; then
-		SPEED=70
+		SPEED="70"
 	elif [ "$TEMP" -le "55" ]; then
-		SPEED=80
+		SPEED="80"
 	elif [ "$TEMP" -le "60" ]; then
-		SPEED=90
+		SPEED="90"
+	elif [ "$TEMP" -gt "60" ]; then
+		SPEED="100"
 	else
-		SPEED=100
+		SPEED="40"
 	fi
 
+	# Changes the fan speed
 	if [ "$SPEED" -ne "$NOW_SPEED" ]; then
 	    nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=""$SPEED"
         NOW_SPEED="$SPEED"
 	fi
 
+	# If you're worried about power usage increase this
 	sleep 2
 done
