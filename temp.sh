9a18c238941f7d943c61f60b2334e7d773f9e69e
Scott Walters <scottwalters97@gmail.com>
2017-07-07 16:15:30 +1000

    Removed a lot of code, making it better overall

--- temp.sh
+++ temp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "~~ nan0s7's fan-speed curve script [version 6] ~~"
+echo "~~ nan0s7's fan-speed curve script [version 7] ~~"
 
 # -- notes --
 # + speed is a percentage value; or { 0..100 }
@@ -16,33 +16,12 @@ SPEED="30"
 # The actual fan curve array; ["TEMP"]="FAN_SPEED_PERCENTAGE"
 declare -a CURVE=( ["40"]="40" ["45"]="50" ["50"]="60" ["55"]="70" ["60"]="85" ["70"]="100" )
 
-# Calculate PC's hostname length upon first run
-PC=`nvidia-settings -q=[gpu:"$GPU"]/GPUCoreTemp`
-for (( i=1; i<=${#PC}; i++ )) do
-	if [ "${PC:i:2}" == ":0" ]; then
-		PC="${PC:28:$[ i - 28 ]}"
-		PC="${#PC}"
-		break
-	fi
-done
-# The number of characters to the actual temp value
-NUM=$[ 28 + ${PC} + 12 ]
-
 # Enable fan control
 nvidia-settings -a "[gpu:""$GPU""]/GPUFanControlState=1"
 
 while [ -z "$LOOP" ]; do
 	# Current temperature query
-	TEMP=`nvidia-settings -q=[gpu:"$GPU"]/GPUCoreTemp`
-
-	# Extracts temperature value from output of command
-	if [ "${TEMP:$[$NUM + 1]:1}" == "." ]; then
-		TEMP="${TEMP:$NUM:1}"
-	elif [ "${TEMP:$[$NUM + 2]:1}" == "." ]; then
-		TEMP="${TEMP:$NUM:2}"
-	else
-		TEMP="${TEMP:$NUM:3}"
-	fi
+	TEMP=`nvidia-settings -q=[gpu:"$GPU"]/GPUCoreTemp -t`
 
 	# Execution of fan curve
 	for VAL in "${!CURVE[@]}"; do
