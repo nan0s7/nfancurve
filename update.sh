ae588163c86000bbe48199e23abc0095322c802f
nan0s7 <greatscotteh@gmail.com>
2017-07-13 19:44:26 +1000

    Added comments and made it slightly smarter

--- update.sh
+++ update.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+echo "~~ nan0s7's fan-speed curve script updater script ~~"
 
 echo "Checking for installed git version..."
-
 verG=`git --version`
 
-if ! [ -z "$verG" ]; then
+# Here I'm assuming that the version string format doesn't change much
+if [ "${#verG}" -lt "22" ]; then
 	echo "Git found!"
 	echo $verG
 else
 	echo "Git not found... you should install that and try again"
-	sleep 3
 	exit
 fi
 
+# Currently installed version
 verL=`cat VERSION.txt`
+# Repository's version
 verR=`git ls-remote -t https://github.com/nan0s7/nfancurve.git`
 verR=${verR:(-3)}
 
@@ -22,20 +24,33 @@ echo "Latest version of script: "$verR
 
 if [ "$verL" == "$verR" ]; then
 	echo "You are using the most up-to-date version"
-	sleep 4
 else
 	echo "You are using an outdated version"
 	echo "If you don't want to automatically download the latest version, press CTRL+C to quit this script"
 	sleep 3
+	# Downloads the whole repository to make sure nothing is old
 	`git clone https://github.com/nan0s7/nfancurve`
 	cd nfancurve
 	echo "Replacing old files"
+	# Removes every file
 	mv -f -t ../ LICENCE README.md temp.desktop temp.sh update.sh USAGE.md VERSION.txt
 	cd ../
 	echo "Removing temporary directory"
+	# Deletes leftover folder
 	rm -rf nfancurve
 	echo "Done"
-	echo "Installed version: "`cat VERSION.txt`
+	echo "Newly installed version: "`cat VERSION.txt`
 fi
+
+# Get's the running PID for my script (if it's running)
+tempPID=`pgrep temp.sh`
+
+# If the outdated script is running, kill it
+if ! [ -z "$tempPID" ]; then
+	kill $tempPID
+fi
+
+# Run the new version of the script
+nohup ./temp.sh &
 
 exit
