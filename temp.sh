c7bf8928b5661420185787872c5fa05bf0cb07b6
nan0s7 <scottwalters97@gmail.com>
2017-08-01 14:47:24 +1000

    Improved algorithm performance & cleaned up code

--- temp.sh
+++ temp.sh
@@ -13,7 +13,10 @@ unset VER
 GPU="0"
 SPEED="25"
 TEMP="0"
-OLD_TEMP="0"
+OLD_TEMP=0
+TDIFF=0
+ERR=3
+ER2=$[ $ERR - 2 ]
 SLP=3
 
 # The actual fan curve array; [TEMP_CELSIUS]=FAN_SPEED_PERCENTAGE
@@ -22,48 +25,46 @@ declare -a CURVE=( ["40"]="25" ["45"]="45" ["50"]="60" ["55"]="70" ["60"]="85" )
 # Enable fan control
 nvidia-settings -a "[gpu:""$GPU""]/GPUFanControlState=1"
 
-# Set initial fan speed
-nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=""$SPEED"
+function set_speed {
+        # Execution of fan curve
+        if [ "$TEMP" -gt "${CURVE[-1]}" ]; then
+                SPEED="100"
+        else
+                for VAL in "${!CURVE[@]}"; do
+                        if [ "$TEMP" -le "$VAL" ]; then
+                                SPEED="${CURVE[$VAL]}"
+                                break
+                        fi
+                done
+        fi
+        # Change the fan speed to the newly calculated one
+        nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=""$SPEED"
+}
 
 while true; do
 	# Current temperature query
 	TEMP=`nvidia-settings -q=[gpu:"$GPU"]/GPUCoreTemp -t`
 
         # Calculate temperature difference
-        OLD_TEMP=$[ $TEMP - $OLD_TEMP ]
+        TDIFF=$[ $TEMP - $OLD_TEMP ]
 
         # Adjust the time between the next reading
-        if [ $OLD_TEMP -ge 5 ]; then
-                SLP=2
-        elif [ $OLD_TEMP -gt 2 ]; then
+        if [ "$TDIFF" -gt "$ERR" ] || [ "$TDIFF" -lt -"$ERR" ]; then
                 SLP=3
-        elif [ $OLD_TEMP -eq 0 ]; then
-                SLP=4
-        elif [ $OLD_TEMP -lt 0 ]; then
+                set_speed
+                OLD_TEMP="$TEMP"
+        elif [ "$TDIFF" -lt "$ER2" ] || [ "$TDIFF" -gt "$ER2" ]; then
+                SLP=6
+        elif [ "$TDIFF" -gt -"$ERR" ]; then
                 SLP=5
+        else
+                SLP=3
+                set_speed
+                OLD_TEMP="$TEMP"
         fi
-
-        if ! [ $OLD_TEMP -gt -5 ]; then
-                # Execution of fan curve
-                if [ "$TEMP" -gt "${CURVE[-1]}" ]; then
-                        SPEED="100"
-                else
-                        for VAL in "${!CURVE[@]}"; do
-                                if [ "$TEMP" -le "$VAL" ]; then
-                                        SPEED="${CURVE[$VAL]}"
-                                        break
-                                fi
-                        done
-                fi
-                # Change the fan speed to the newly calculated one
-                nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=""$SPEED"
-        fi
-
-        # Set this to the old temperature reading
-        OLD_TEMP=$TEMP
 
 	# This will automatically adjust
-	sleep $SLP
+	sleep "$SLP"
 done
 
 # Make sure the variables are back to normal
@@ -74,3 +75,6 @@ unset OLD_TEMP
 unset SLP
 unset SPEED
 unset VAL
+unset TDIFF
+unset ERR
+unset ER2
