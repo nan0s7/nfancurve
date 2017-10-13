1884c4e93927a1f0c6f5905e9f5fd1da69b1b112
nan0s7 <scottwalters97@gmail.com>
2017-10-13 22:00:54 +1100

    Modified structure and improved code

--- update.sh
+++ update.sh
@@ -1,59 +1,94 @@
 #!/bin/bash
 echo "~~ nan0s7's fan-speed curve script updater script ~~"
 
-# This script is able to run temp.sh in the background for you, btw
-echo "Checking for installed git version..."
-verG=`git --version`
+function finish {
+	unset git_version
+	unset local_version
+	unset repo_version
+	unset temp_pid
+	unset temp_count
+}
+trap finish EXIT
 
-# Here I'm assuming that the version string format does not change much
-if [ "${#verG}" -lt "22" ]; then
-	echo "Git found!"
-	echo $verG
-else
-	echo "Git not found... you should install that and try again"
-	exit
-fi
+function check_for_git {
+	echo -e "\nChecking if git is installed..."
+	git_version=`git --version`
 
-# Currently installed version
-verL=`cat VERSION.txt`
-# Repository's version
-verR=`git ls-remote -t https://github.com/nan0s7/nfancurve.git`
-verR=${verR:(-3)}
+	# Here I'm assuming that the version string format does not change much
+	if [ "${#git_version}" -lt "22" ]; then
+		echo "Git found!"
+		echo $git_version
+	else
+		echo "Git not found... you should install that and try again"
+		exit
+	fi
+}
 
-echo "Installed version of script: "$verL
-echo "Latest version of script: "$verR
+function get_nfancurve_version {
+    # Currently installed version
+    local_version=`cat VERSION.txt`
+    # Repository's version
+    repo_version=`git ls-remote -t https://github.com/nan0s7/nfancurve.git`
+    repo_version=${repo_version:(-3)}
 
-if [ "$verL" -eq "$verR" ]; then
-	echo "You are using the most up-to-date version"
-elif [ "$verL" -lt "$verR" ]; then
-	echo "You are using an outdated version"
-	echo "If you don't want to automatically download the latest version, press CTRL+C to quit this script"
-	sleep 4
-	# Downloads the whole repository to make sure nothing is old
-	`git clone https://github.com/nan0s7/nfancurve`
-	cd nfancurve
-	echo "Replacing old files"
-	# Moves every file into main directory
-	mv -f -t ../ `ls`
-	cd ../
-	echo "Removing temporary directory"
-	# Deletes leftover folder
-	rm -rf nfancurve
-	echo "Done"
-	echo "Newly installed version: "`cat VERSION.txt`
-else
-	echo "You're using a developer version! =O"
-fi
+    echo ""
+    echo "Installed version of script: "$local_version
+    echo "Latest version of script: "$repo_version
+}
 
-# Gets the running PID for my script (if it's running)
-tempPID=`pgrep temp.sh`
+function update_everything {
+    if [ "$local_version" -eq "$repo_version" ]; then
+	    echo "You are using the most up-to-date version"
+    elif [ "$local_version" -lt "$repo_version" ]; then
+	    echo "You are using an outdated version"
+	    echo "If you don't want to automatically download the latest version, press CTRL+C to quit this script"
+	    sleep 4
 
-# If the outdated script is running, kill it
-if [ -n "$tempPID" ] && [ "$tempPID" -gt 0 ]; then
-	kill $tempPID
-fi
+	    # Downloads the whole repository to make sure nothing is old
+	    `git clone https://github.com/nan0s7/nfancurve`
+	    cd nfancurve
+	    echo "Replacing old files"
+	    # Moves every file into main directory
+	    mv -f -t ../ `ls`
+	    cd ../
+	    echo "Removing temporary directory"
+	    # Deletes leftover folder
+	    rm -rf nfancurve
+	    echo "Done"
+	    echo "Newly installed version: "`cat VERSION.txt`
+    else
+	    echo "You're using a developer version! =O"
+    fi
+}
 
-# Run the new version of the script
-nohup ./temp.sh >/dev/null 2>&1 &
+function kill_already_running {
+    # Gets the running PID for my script (if it's running)
+    temp_count=`pgrep -c temp.sh`
+    echo -e "\nThis code was taken from my other script, called; nron.sh"
+    echo "Check out https://github.com/nan0s7/nrunornot for more info!"
 
-exit
+    if [ "$temp_count" -gt "0" ]; then
+	    echo -e "\nNumber of currently running temp.sh processes: ""$temp_count"
+	    for i in `seq 1 $temp_count`; do
+		    temp_pid=`pgrep -o temp.sh`
+		    echo -e "Killing process... "$temp_pid"\n"
+		    kill $temp_pid
+	    done
+    fi
+}
+
+function main {
+    check_for_git
+    get_nfancurve_version
+
+    update_everything
+    kill_already_running
+
+    # Run the new version of the script
+    nohup ./temp.sh >/dev/null 2>&1 &
+    echo "A new background process was started."
+
+    exit
+}
+
+main
