466ea699715cba763b733213568df2f5a31633a5
Scott <greatscotteh@gmail.com>
2018-06-19 23:15:12 +1000

    Changed structure of config because it's a smarter way of doing it

---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+min_temp="0"
+max_temp="110"
+tdiff_avg_or_max="1"
+declare -a slp_times=( "7" "5" )
+declare -a fcurve=( "25" "40" "55" "70" "85" )
+declare -a tcurve=( "35" "45" "50" "55" "60" )
